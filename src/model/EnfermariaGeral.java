3b79bc5ff3608ca1f8e53a19fc3182835f11436c
rosarinhobarros <up202506863@up.pt>
2026-04-26 16:08:16 +0100

    Update EnfermariaGeral.java

--- src/model/EnfermariaGeral.java
+++ src/model/EnfermariaGeral.java
@@ -3,6 +3,15 @@ package model;
 public class EnfermariaGeral extends Enfermaria {
     private int acompanhantes;
     private String recursos;
+    
+    /**
+     * Cria uma enfermaria geral.
+     *
+     * @param id identificador
+     * @param camas número de camas
+     * @param acompanhantes número permitido de acompanhantes
+     * @param recursos recursos disponíveis
+     */
 
     public EnfermariaGeral(String id, int camas, int acompanhantes, String recursos) {
         super(id, camas);
