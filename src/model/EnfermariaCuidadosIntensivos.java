509211ba5844489e8a3b209f54ba946999044eba
rosarinhobarros <up202506863@up.pt>
2026-04-26 16:06:36 +0100

    Update EnfermariaCuidadosIntensivos.java

--- src/model/EnfermariaCuidadosIntensivos.java
+++ src/model/EnfermariaCuidadosIntensivos.java
@@ -5,6 +5,16 @@ public class EnfermariaCuidadosIntensivos extends Enfermaria {
     private double pressao;
     private double referencia;
 
+    /**
+     * Cria uma unidade de cuidados intensivos.
+     *
+     * @param id identificador
+     * @param camas número de camas
+     * @param horarioVisitas horário de visitas
+     * @param pressao pressão atmosférica
+     * @param referencia pressão atmosférica de referência
+     */
+
     public EnfermariaCuidadosIntensivos(String id, int camas, String horarioVisitas, double pressao, double referencia) {
         super(id, camas);
         this.horarioVisitas = horarioVisitas;
